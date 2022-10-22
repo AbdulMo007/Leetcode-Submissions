cf6c903a0619849ec234e81755f52d62e08d0f50
AbdulMo007 <85588168+AbdulMo007@users.noreply.github.com>
2022-10-22 16:02:07 +1100

    Time: 71 ms (11.03%), Space: 13.9 MB (72.23%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -8,14 +8,17 @@ class Solution:
         
         # if s1 == s2:
         #     return True
-        
+    
         if Counter(s1) != Counter(s2):
             return False 
         
         for i in range (len(s1)):
             if s1[i]!=s2[i]:
                 c += 1
+            if c>2:
+                return False 
         return c <= 2
+        
         
         
         
