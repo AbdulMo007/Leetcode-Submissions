1f96052ea41c4b3901a421ac654bd4f27e4cf910
AbdulMo007 <85588168+AbdulMo007@users.noreply.github.com>
2022-10-26 09:33:08 +1100

    Time: 94 ms (46.57%), Space: 14 MB (58.18%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.py
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -1,11 +1,11 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        s = ""
-        for i in arr:
-            s += str(i%2)
-        return "111" in s
+        # s = ""
+        # for i in arr:
+        #     s += str(i%2)
+        # return "111" in s
     
-        # return "111" in "".join([str(i%2) for i in arr])
+         return "111" in "".join([str(i%2) for i in arr])
         
 #         for i in range (len(arr)-2):
 #             if arr[i] % 2 == 1:
