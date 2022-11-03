2ac24ca28c2797fd016b2ffcbd67a44637eb273f
AbdulMo007 <85588168+AbdulMo007@users.noreply.github.com>
2022-11-04 10:13:43 +1100

    Time: 149 ms (38.56%), Space: 15 MB (69.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,10 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         D = {}
         
-        for ind,val in enumerate(nums):
-            R = target - val 
+        for ind, val in enumerate(nums):
+            R = target - val
             if R in D:
-                return D[R], ind 
-            D[val] = ind
-                
+                return D[R], ind
+            D [val] = ind              
             
