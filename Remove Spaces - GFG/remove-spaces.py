41b0c16c0f50df9e8c44fe679540f4611287907d
AbdulMo007 <85588168+AbdulMo007@users.noreply.github.com>
2022-11-06 13:29:36 +1100

    Added solution - LeetHub

---
+++ Remove Spaces - GFG/remove-spaces.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def modify(self, s):
+        s = s.split(" ")
+        s = "".join(s)  
+        return s
+        
+        # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        ob = Solution()
+        ans = ob.modify(s)
+        print(ans)
+# } Driver Code Ends
