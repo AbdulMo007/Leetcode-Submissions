ce1af6738da3079f6f2f5275854c8d5b6a5ab678
AbdulMo007 <85588168+AbdulMo007@users.noreply.github.com>
2022-11-06 11:04:12 +1100

    Added solution - LeetHub

---
+++ Count of smaller elements - GFG/count-of-smaller-elements.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+def countOfElements( a, n, x):
+    num = 0
+    for i in range (0,n):
+        if a[i]<=x:
+            num += 1
+    return num
+            
+            
+        
+        
+        
+    
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        k = int(input())
+        
+        print(countOfElements(a, n, k))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+# } Driver Code Ends
