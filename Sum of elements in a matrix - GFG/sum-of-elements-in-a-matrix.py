56ebb005b1754d342673bcef389f683dbcdcb341
AbdulMo007 <85588168+AbdulMo007@users.noreply.github.com>
2022-11-10 10:00:25 +1100

    Added solution - LeetHub

---
+++ Sum of elements in a matrix - GFG/sum-of-elements-in-a-matrix.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:
+    def sumOfMatrix(self,N,M,Grid):
+        sum = 0
+        for i in range (N):
+            for j in range (M):
+                sum += (Grid[i][j])
+        return sum
+                
+
+            
+#Grid=
+# [[1,0,1],
+# [-8,9,-2]]
+
+
+            
+        #code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        N,M=map(int,input().strip().split(' '))
+        Grid = [[0 for i in range(M)] for j in range(N)]
+        for i in range(N):
+            s=list(map(int,input().strip().split(' ')))
+            for j in range(M):
+                Grid[i][j]=s[j]
+        ob=Solution()
+        print(ob.sumOfMatrix(N,M,Grid)) 
+# } Driver Code Ends
