8321784ea715d265f326514884964598a73fef3f
AbdulMo007 <85588168+AbdulMo007@users.noreply.github.com>
2022-11-13 10:53:28 +1100

    Time: 862 ms (24.36%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,12 +1,5 @@
-# SELECT Name as Customers
-# FROM Customers
-# JOIN 
-
-
-
-
 SELECT name as Customers
 FROM Customers 
-WHERE Id NOT IN (SELECT customerId
-FROM Orders);
-
+WHERE id NOT IN
+(SELECT customerId 
+FROM orders) 
